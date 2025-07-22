05eb798e83205a11a69239d56e72c7b4c3993a88
lequangminh2623 <lequangminh2623@gmail.com>
2025-07-22 16:35:57 +0700

    init project

---
+++ studentgradeapp/src/main/java/com/lqm/studentgradeapp/StudentgradeappApplication.java
@@ -0,0 +1,13 @@
+package com.lqm.studentgradeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentgradeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentgradeappApplication.class, args);
+    }
+
+}
